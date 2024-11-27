d783fc7882149a114d0702a129d6cb7451b17dce
TheObtey <95309102+TheObtey@users.noreply.github.com>
2024-11-27 12:38:53 +0100

    Update sh_functions.lua
    Modify NPC_ARMORY.GetSentence() in adequation with the new language table

--- npc_sell_weapon/lua/npc_sell_weapon/shared/sh_functions.lua
+++ npc_sell_weapon/lua/npc_sell_weapon/shared/sh_functions.lua
@@ -1,12 +1,12 @@
 function NPC_ARMORY.GetSentence(key)
     local result = "Language avaible : en | fr | es | ru | de"
 
-    local sentence = istable(NPC_ARMORY.Language[NPC_ARMORY.Lang]) and NPC_ARMORY.Language[NPC_ARMORY.Lang][key] or nil
+    local sentence = istable(NPC_ARMORY.Languages[NPC_ARMORY.Lang]) and NPC_ARMORY.Languages[NPC_ARMORY.Lang][key] or nil
 
-    if istable(NPC_ARMORY.Language[NPC_ARMORY.Lang]) and isstring(sentence) then
+    if istable(NPC_ARMORY.Languages[NPC_ARMORY.Lang]) and isstring(sentence) then
         result = sentence 
-    elseif istable(NPC_ARMORY.Language["en"]) and isstring(NPC_ARMORY.Language["en"][key]) then
-        result = NPC_ARMORY.Language["en"][key]
+    elseif istable(NPC_ARMORY.Languages["en"]) and isstring(NPC_ARMORY.Languages["en"][key]) then
+        result = NPC_ARMORY.Languages["en"][key]
     end
 
     return result
